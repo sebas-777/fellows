c0d5e2e1c3afd1252e6b5fcaccc8e8b9c7d865a7
sebas777 <sebastiancast06@hotmail.com>
2026-02-16 23:51:40 -0500

    :construction:{sc]created header  component

--- Frontend/Fellows/Fellows/src/app/app.ts
+++ Frontend/Fellows/Fellows/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { Header } from "./layout/header/header";
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [Header]
 })
 export class App {
   protected readonly title = signal('Fellows');
